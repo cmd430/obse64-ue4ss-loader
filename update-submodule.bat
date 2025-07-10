0444eb92f7a48ed882538cab5a3c837ba7e6accf
cmd430 <cmd430@gmail.com>
2025-07-10 15:51:27 +1200

    update deps and fix weird issue with update batch

--- update-submodule.bat
+++ update-submodule.bat
@@ -1,3 +1,6 @@
 @echo off
-git submodule update --init --recursive -f
-git submodule update --remote -f
+git submodule update --init --recursive --remote --force
+
+
+
+rem git clone --recurse-submodules https://github.com/libxse/commonlibob64
